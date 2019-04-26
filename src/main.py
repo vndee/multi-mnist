54812535a41ff0855589c85bdb33f3b8b7fd1a53
Duy Huynh <hvd.huynhduy@gmail.com>
2019-04-26 14:15:56 +0700

    update main.py

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ output_dir = '../output'
 label_file = 'labels.csv'
 
 os.makedirs(output_dir, exist_ok=True)
+n_samples = [100000, 100000, 100000, 100000, 1000000, 1000000, 2000000, 3000000, 4000000]
 
 cnt = 0
 number_of_samples_per_class = 10
@@ -184,13 +185,13 @@ if __name__ == '__main__':
     print('--------------Generate train set-----------------')
     writer = open(os.path.join(output_dir, 'train', label_file), 'w+')
     for num_digits in range(1, 10):
-        generator(train_file, train_label, num_digits, number_of_samples_per_class, writer, name='train')
+        generator(train_file, train_label, num_digits, n_samples[num_digits], writer, name='train')
     writer.close()
     print('-------------------------------------------------\n')
 
     print('--------------Generate test set------------------')
     writer = open(os.path.join(output_dir, 'test', label_file), 'w+')
     for num_digits in range(1, 10):
-        generator(test_file, test_label, num_digits, number_of_samples_per_class, writer, name='test')
+        generator(test_file, test_label, num_digits, n_samples[num_digits], writer, name='test')
     writer.close()
     print('-------------------------------------------------\n')
